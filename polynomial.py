1c5502ac0ffe83e7096f39d25cc50cdfc48debe0
Kian Boon <kianboon@yctan.com>
2024-01-28 23:05:56 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -1,4 +1,6 @@
 class X:
+    """ This class represents the X variable in a polynomial.
+    """
     def __init__(self):
         pass
 
@@ -6,6 +8,8 @@ class X:
         return "X"
 
 class Int:
+    """ This class represents any standalone integers in a polynomial.
+    """
     def __init__(self, i):
         self.i = i
     
@@ -13,27 +17,70 @@ class Int:
         return str(self.i)
 
 class Add:
+    """ This class represents adding two elements in a polynomial together.
+    """
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
-
-class Mul:
+    
+class Subtract:
+    """ This class represents subtracting element p2 from element p1 in a polynomial.
+    """
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
     
     def __repr__(self):
-        if isinstance(self.p1, Add):
-            if isinstance(self.p2, Add):
-                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
+        return repr(self.p1) + " - " + repr(self.p2)
+
+class Mul:
+    """ This class represents multiplying two elements in a polynomial together.
+    """
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        # Respect order of operations when both arguments include + / -.
+        if isinstance(self.p1, (Add, Subtract)) and isinstance(self.p2, (Add, Subtract)):
+            return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
+
+        # Respect order of operations when only p1 includes + / -.
+        if isinstance(self.p1, (Add, Subtract)):
             return "( " + repr(self.p1) + " ) * " + repr(self.p2)
-        if isinstance(self.p2, Add):
+        
+        # Respect order of operations when only p2 includes + / -.
+        if isinstance(self.p2, (Add, Subtract)):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
+        
+        # Otherwise, we can simply return without any brackets.
         return repr(self.p1) + " * " + repr(self.p2)
 
+class Div:
+    """ This class represents dividing element p1 by element p2 in a polynomial.
+    """
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def __repr__(self):
+        # Respect order of operations when both arguments include + / -.
+        if isinstance(self.p1, (Add, Subtract)) and isinstance(self.p2, (Add, Subtract)):
+            return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
+
+        # Respect order of operations when only p1 includes + / -.
+        if isinstance(self.p1, (Add, Subtract)):
+            return "( " + repr(self.p1) + " ) / " + repr(self.p2)
+        
+        # Respect order of operations when only p2 includes + / -.
+        if isinstance(self.p2, (Add, Subtract)):
+            return repr(self.p1) + " / ( " + repr(self.p2) + " )"
+        
+        # Otherwise, we can simply return without any brackets.
+        return repr(self.p1) + " / " + repr(self.p2)
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
